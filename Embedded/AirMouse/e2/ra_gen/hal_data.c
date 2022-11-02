a34b8bca2730a73ca458d57c1c8fb0ffcf2d6ef8
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2022-11-02 23:02:31 +0900

    Add files via upload

---
+++ Embedded/AirMouse/e2/ra_gen/hal_data.c
@@ -0,0 +1,169 @@
+/* generated HAL source file - do not edit */
+#include "hal_data.h"
+iic_master_instance_ctrl_t g_i2c_master0_ctrl;
+const iic_master_extended_cfg_t g_i2c_master0_extend =
+{ .timeout_mode = IIC_MASTER_TIMEOUT_MODE_SHORT, .timeout_scl_low = IIC_MASTER_TIMEOUT_SCL_LOW_ENABLED,
+/* Actual calculated bitrate: 98425. Actual calculated duty cycle: 50%. */.clock_settings.brl_value = 28,
+  .clock_settings.brh_value = 28, .clock_settings.cks_value = 3, };
+const i2c_master_cfg_t g_i2c_master0_cfg =
+{ .channel = 0, .rate = I2C_MASTER_RATE_STANDARD, .slave = 0x68, .addr_mode = I2C_MASTER_ADDR_MODE_7BIT,
+#define RA_NOT_DEFINED (1)
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_tx = NULL,
+#else
+                .p_transfer_tx       = &RA_NOT_DEFINED,
+#endif
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_rx = NULL,
+#else
+                .p_transfer_rx       = &RA_NOT_DEFINED,
+#endif
+#undef RA_NOT_DEFINED
+  .p_callback = iic_callback,
+  .p_context = NULL,
+#if defined(VECTOR_NUMBER_IIC0_RXI)
+    .rxi_irq             = VECTOR_NUMBER_IIC0_RXI,
+#else
+  .rxi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_IIC0_TXI)
+    .txi_irq             = VECTOR_NUMBER_IIC0_TXI,
+#else
+  .txi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_IIC0_TEI)
+    .tei_irq             = VECTOR_NUMBER_IIC0_TEI,
+#else
+  .tei_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_IIC0_ERI)
+    .eri_irq             = VECTOR_NUMBER_IIC0_ERI,
+#else
+  .eri_irq = FSP_INVALID_VECTOR,
+#endif
+  .ipl = (12),
+  .p_extend = &g_i2c_master0_extend, };
+/* Instance structure to use this module. */
+const i2c_master_instance_t g_i2c_master0 =
+{ .p_ctrl = &g_i2c_master0_ctrl, .p_cfg = &g_i2c_master0_cfg, .p_api = &g_i2c_master_on_iic };
+#include "r_sci_i2c_cfg.h"
+sci_i2c_instance_ctrl_t g_i2c0_ctrl;
+const sci_i2c_extended_cfg_t g_i2c0_cfg_extend =
+        {
+        /* Actual calculated bitrate: 99981. Actual SDA delay: 300 ns. */.clock_settings.clk_divisor_value = 0,
+          .clock_settings.brr_value = 20, .clock_settings.mddr_value = 172, .clock_settings.bitrate_modulation = true, .clock_settings.cycles_value =
+                  30,
+          .clock_settings.snfr_value = (1), };
+
+const i2c_master_cfg_t g_i2c0_cfg =
+{ .channel = 9, .rate = I2C_MASTER_RATE_STANDARD, .slave = 0x68, .addr_mode = I2C_MASTER_ADDR_MODE_7BIT,
+#define RA_NOT_DEFINED (1)
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_tx = NULL,
+#else
+    .p_transfer_tx       = &RA_NOT_DEFINED,
+#endif
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_rx = NULL,
+#else
+    .p_transfer_rx       = &RA_NOT_DEFINED,
+#endif
+#undef RA_NOT_DEFINED
+  .p_callback = sci_i2c_master_callback,
+  .p_context = NULL,
+#if defined(VECTOR_NUMBER_SCI9_RXI) && SCI_I2C_CFG_DTC_ENABLE
+    .rxi_irq             = VECTOR_NUMBER_SCI9_RXI,
+#else
+  .rxi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI9_TXI)
+    .txi_irq             = VECTOR_NUMBER_SCI9_TXI,
+#else
+  .txi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI9_TEI)
+    .tei_irq             = VECTOR_NUMBER_SCI9_TEI,
+#else
+  .tei_irq = FSP_INVALID_VECTOR,
+#endif
+  .ipl = (12), /* (BSP_IRQ_DISABLED) is unused */
+  .p_extend = &g_i2c0_cfg_extend, };
+/* Instance structure to use this module. */
+const i2c_master_instance_t g_i2c0 =
+{ .p_ctrl = &g_i2c0_ctrl, .p_cfg = &g_i2c0_cfg, .p_api = &g_i2c_master_on_sci };
+sci_uart_instance_ctrl_t g_uart0_ctrl;
+
+baud_setting_t g_uart0_baud_setting =
+        {
+        /* Baud rate calculated with 0.469% error. */.semr_baudrate_bits_b.abcse = 0,
+          .semr_baudrate_bits_b.abcs = 0, .semr_baudrate_bits_b.bgdm = 1, .cks = 0, .brr = 53, .mddr = (uint8_t) 256, .semr_baudrate_bits_b.brme =
+                  false };
+
+/** UART extended configuration for UARTonSCI HAL driver */
+const sci_uart_extended_cfg_t g_uart0_cfg_extend =
+{ .clock = SCI_UART_CLOCK_INT, .rx_edge_start = SCI_UART_START_BIT_FALLING_EDGE, .noise_cancel =
+          SCI_UART_NOISE_CANCELLATION_DISABLE,
+  .rx_fifo_trigger = SCI_UART_RX_FIFO_TRIGGER_MAX, .p_baud_setting = &g_uart0_baud_setting, .flow_control =
+          SCI_UART_FLOW_CONTROL_RTS,
+#if 0xFF != 0xFF
+                .flow_control_pin       = BSP_IO_PORT_FF_PIN_0xFF,
+                #else
+  .flow_control_pin = (bsp_io_port_pin_t) UINT16_MAX,
+#endif
+  .rs485_setting =
+  { .enable = SCI_UART_RS485_DISABLE, .polarity = SCI_UART_RS485_DE_POLARITY_HIGH,
+#if 0xFF != 0xFF
+                    .de_control_pin = BSP_IO_PORT_FF_PIN_0xFF,
+                #else
+    .de_control_pin = (bsp_io_port_pin_t) UINT16_MAX,
+#endif
+          }, };
+
+/** UART interface configuration */
+const uart_cfg_t g_uart0_cfg =
+{ .channel = 0, .data_bits = UART_DATA_BITS_8, .parity = UART_PARITY_OFF, .stop_bits = UART_STOP_BITS_1, .p_callback =
+          user_uart_callback,
+  .p_context = NULL, .p_extend = &g_uart0_cfg_extend,
+#define RA_NOT_DEFINED (1)
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_tx = NULL,
+#else
+                .p_transfer_tx       = &RA_NOT_DEFINED,
+#endif
+#if (RA_NOT_DEFINED == RA_NOT_DEFINED)
+  .p_transfer_rx = NULL,
+#else
+                .p_transfer_rx       = &RA_NOT_DEFINED,
+#endif
+#undef RA_NOT_DEFINED
+  .rxi_ipl = (12),
+  .txi_ipl = (12), .tei_ipl = (12), .eri_ipl = (12),
+#if defined(VECTOR_NUMBER_SCI0_RXI)
+                .rxi_irq             = VECTOR_NUMBER_SCI0_RXI,
+#else
+  .rxi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI0_TXI)
+                .txi_irq             = VECTOR_NUMBER_SCI0_TXI,
+#else
+  .txi_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI0_TEI)
+                .tei_irq             = VECTOR_NUMBER_SCI0_TEI,
+#else
+  .tei_irq = FSP_INVALID_VECTOR,
+#endif
+#if defined(VECTOR_NUMBER_SCI0_ERI)
+                .eri_irq             = VECTOR_NUMBER_SCI0_ERI,
+#else
+  .eri_irq = FSP_INVALID_VECTOR,
+#endif
+        };
+
+/* Instance structure to use this module. */
+const uart_instance_t g_uart0 =
+{ .p_ctrl = &g_uart0_ctrl, .p_cfg = &g_uart0_cfg, .p_api = &g_uart_on_sci };
+void g_hal_init(void)
+{
+    g_common_init ();
+}
