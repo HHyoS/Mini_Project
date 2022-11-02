4b63c97fac1792273b65dabcde75aa11042316e9
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2022-11-02 23:01:06 +0900

    Add files via upload

---
+++ Embedded/AirMouse/e2/Debug/ra/fsp/src/bsp/mcu/all/bsp_rom_registers.d
@@ -0,0 +1,86 @@
+ra/fsp/src/bsp/mcu/all/bsp_rom_registers.o: \
+ ../ra/fsp/src/bsp/mcu/all/bsp_rom_registers.c \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/bsp_api.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/fsp_common_api.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/fsp_version.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_gen/bsp_clock_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_mcu_info.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_elc.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_feature.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board_init.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board_leds.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_gen/vector_data.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA6E10F.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm33.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_common_api.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_irq.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_io.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_clocks.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_security.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_features.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_common_api.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_delay.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/bsp_api.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/fsp_common_api.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/fsp_version.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_gen/bsp_clock_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_mcu_info.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_elc.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_feature.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board_init.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board_leds.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_gen/vector_data.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA6E10F.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm33.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_common.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_common_api.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_irq.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_io.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_clocks.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_security.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_features.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_common_api.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_delay.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h:
---
+++ Embedded/AirMouse/e2/Debug/ra_gen/hal_data.d
@@ -0,0 +1,116 @@
+ra_gen/hal_data.o: ../ra_gen/hal_data.c ../ra_gen/hal_data.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/bsp_api.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/fsp_common_api.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/fsp_version.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_gen/bsp_clock_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_mcu_info.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_elc.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_feature.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board_init.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board_leds.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_gen/vector_data.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA6E10F.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm33.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_common_api.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_irq.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_io.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_clocks.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_security.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_features.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_common_api.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_delay.h \
+ c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h \
+ ../ra_gen/common_data.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/instances/r_ioport.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/r_ioport_api.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/bsp_api.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/r_ioport_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/instances/r_iic_master.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/r_iic_master_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/r_i2c_master_api.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/r_transfer_api.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/instances/r_sci_i2c.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/r_sci_i2c_cfg.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/instances/r_sci_uart.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/r_uart_api.h \
+ C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/r_sci_uart_cfg.h
+../ra_gen/hal_data.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/bsp_api.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/fsp_common_api.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/fsp_version.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_gen/bsp_clock_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_mcu_info.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_elc.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\ra6e1\bsp_feature.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board_init.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\board\ra6e1_fpb\board_leds.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_gen/vector_data.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA6E10F.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm33.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_common.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_common_api.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_irq.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_io.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_clocks.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_security.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_features.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\inc\fsp_common_api.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_delay.h:
+c:\users\multicampus\e2_studio\workspace\bhc\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h:
+../ra_gen/common_data.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/instances/r_ioport.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/r_ioport_api.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/bsp_api.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/r_ioport_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/instances/r_iic_master.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/r_iic_master_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/r_i2c_master_api.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/r_transfer_api.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/instances/r_sci_i2c.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/r_sci_i2c_cfg.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/instances/r_sci_uart.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra/fsp/inc/api/r_uart_api.h:
+C:/Users/multicampus/e2_studio/workspace/bhc/ra_cfg/fsp_cfg/r_sci_uart_cfg.h:
